2a3d0791f2f0860fd9b145582762b8a86fdf052f
anastasiia-kovalchuk <anastasiyakov99@gmail.com>
2024-02-29 18:09:19 +0200

    Refactored countdown timer.

--- js/timer.js
+++ js/timer.js
@@ -6,25 +6,26 @@ const getDurationObject = (duration) => {
   let minutes = Math.floor((duration % 3600) / 60);
   let seconds = Math.floor(duration % 60);
 
-  return {hours, minutes,  seconds}
-}
+  return { hours, minutes, seconds };
+};
 const formatDurationObject = ({ hours, minutes, seconds }) => {
   minutes = minutes < 10 ? `0${minutes}` : minutes;
   seconds = seconds < 10 ? `0${seconds}` : seconds;
 
   return { hours, minutes, seconds };
 };
-const showDuration = (timerElement, { hours, minutes, seconds }) => {
+const showDuration = (timerElement, duration) => {
+  const durationObject = getDurationObject(duration);
+  const formattedDurationObject = formatDurationObject(durationObject);
+  const {hours, minutes, seconds} = formattedDurationObject
+
   timerElement.innerHTML = `${hours}:${minutes}:${seconds}`;
 };
 const startCountdownTimer = (timerElement, duration) => {
   let count = duration;
-  const timer = setInterval(()=> {
+  const timer = setInterval(() => {
     count--;
-    const durationObject = getDurationObject(count);
-    const formattedDurationObject = formatDurationObject(durationObject);
-    showDuration(timerElement, formattedDurationObject);
-    // console.log(count);
+    showDuration(timerElement, count);
     if (count === 0) {
       clearInterval(timer);
     }
@@ -37,8 +38,6 @@ const initialDurationObject = {
   minutes: 0,
   seconds: 0
 };
-const formattedDurationObject = formatDurationObject(initialDurationObject);
-showDuration(timerElement, formattedDurationObject);
-
 const duration = getDurationInSeconds(initialDurationObject);
-startCountdownTimer(timerElement ,duration);
+showDuration(timerElement, duration);
+startCountdownTimer(timerElement, duration);
